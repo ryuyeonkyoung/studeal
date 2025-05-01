a9025b8289a555fa23d4ecca275a538b48f67250
류연경 <ykr.dev@gmail.com>
2025-05-01 22:05:52 +0900

    feat: Spring Boot 프로젝트 초기 설정
    - 기본 의존성 추가
    - Gradle Wrapper 설정
    - 메인 애플리케이션 및 테스트 클래스 생성
    - 기본 .gitignore 추가

---
+++ src/main/java/com/studeal/team/StuDealApplication.java
@@ -0,0 +1,13 @@
+package com.studeal.team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuDealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuDealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studeal/team/StuDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studeal.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StuDealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
