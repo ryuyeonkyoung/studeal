bba962b2dbd835645b0b95fbc7dde2868afd0d8f
류연경 <ykr.dev@gmail.com>
2025-05-27 23:19:47 +0900

    feat: LessonService에 에러 핸들러 적용

--- src/main/java/com/studeal/team/global/error/exception/handler/LessonHandler.java
+++ src/main/java/com/studeal/team/global/error/exception/handler/LessonHandler.java
@@ -3,8 +3,8 @@ package com.studeal.team.global.error.exception.handler;
 import com.studeal.team.global.error.code.status.ErrorStatus;
 import com.studeal.team.global.error.exception.GeneralException;
 
-public class CourseHandler extends GeneralException {
-    public CourseHandler(ErrorStatus status) {
+public class LessonHandler extends GeneralException {
+    public LessonHandler(ErrorStatus status) {
         super( status);
     }
 }
