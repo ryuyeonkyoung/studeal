8eff95abc882ff111d28764da61048e00efb1358
류연경 <ykr.dev@gmail.com>
2025-05-28 00:51:46 +0900

    feat: 강화된 이메일 검증 기능 추가 (@StrictEmail)
    - @StrictEmail 커스텀 어노테이션 및 StrictEmailValidator 구현
    - RFC 5322 기반 정규식 패턴으로 이메일 형식 검증
    - 도메인 및 최상위 도메인(TLD) 유효성 검사 적용

---
+++ src/main/java/com/studeal/team/global/validation/annotation/StrictEmail.java
@@ -0,0 +1,16 @@
+package com.studeal.team.global.validation.annotation;
+
+import com.studeal.team.global.validation.validator.StrictEmailValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = StrictEmailValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface StrictEmail {
+    String message() default "유효하지 않은 이메일 형식입니다";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
