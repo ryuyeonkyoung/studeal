7af5a411b4c6ab52f3fd0bb4457f4f03893a7b4f
류연경 <ykr.dev@gmail.com>
2025-06-03 18:27:57 +0900

    docs(enrollment): Enrollment 관련 명세 수정 #2
    - 관련 설명 보완
    - WAITING → CONFIRMED/CANCELED, CONFIRMED → CANCELED 방향의 변경만 가능

--- src/main/java/com/studeal/team/domain/enrollment/api/EnrollmentController.java
+++ src/main/java/com/studeal/team/domain/enrollment/api/EnrollmentController.java
@@ -23,11 +23,12 @@ public class EnrollmentController {
 
     @Operation(
             summary = "최고가를 부른 학생과 선생님을 연결하는 API",
-            description = "경매 종료 후, 최고가를 부른 학생과 선생님을 연결하는 API입니다. 이를 통해 학생은 낙찰받은 수업을 결제하여 거래를 성사하거나, 거절하여 최종 낙찰을 포기할 수 있습니다.")
+            description = "경매 종료 후, 최고가를 부른 학생과 선생님을 연결하는 API입니다." +
+                    "이를 통해 학생은 낙찰받은 수업을 결제하여 거래를 성사하거나, 거절하여 최종 낙찰을 포기할 수 있습니다.")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "STUDENT4001", description = "존재하지 않는 학생입니다.", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "NEGOTIATION404", description = "존재하지 않는 협상입니다.", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "ENROLLMENT4001", description = "이미 수강 신청이 존재합니다.", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "ENROLLMENT4001", description = "이미 Enrollment(학생의 수업 참여 확정 현황)가 존재합니다.", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "ENROLLMENT4003", description = "협상이 성공 상태가 아닙니다.", content = @Content(schema = @Schema(implementation = ApiResponse.class)))
     })
     @PostMapping
@@ -37,9 +38,10 @@ public class EnrollmentController {
 
     @Operation(
             summary = "수업 참여 확정 변경 API",
-            description = "수업 참여 확정 상태를 변경하는 API입니다. (WAITING, CONFIRMED, CANCELED 상태로 변경 가능)")
+            description = "수업 참여 확정 상태를 변경하는 API입니다. (WAITING → CONFIRMED/CANCELED, CONFIRMED → CANCELED 변경 가능)" +
+                    " 이 API를 통해 학생은 수업 참여 확정 상태를 업데이트할 수 있습니다.")
     @ApiResponses({
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "ENROLLMENT404", description = "존재하지 않는 수강 신청입니다.", content = @Content(schema = @Schema(implementation = ApiResponse.class)))
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "ENROLLMENT404", description = "존재하지 않는 수강 신청입니다.", content = @Content(schema = @Schema(implementation = ApiResponse.class))),
     })
     @PatchMapping("/{enrollmentId}/status")
     public ApiResponse<EnrollmentResponseDTO> updateStatus(
