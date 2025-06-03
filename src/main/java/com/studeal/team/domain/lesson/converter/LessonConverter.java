598ade8ba469ce66ac08b2469601415f48df0dab
류연경 <ykr.dev@gmail.com>
2025-06-03 19:44:57 +0900

    refactor(enrollment): 유틸리티 클래스 LessonConverter 설계 개선
    - final 클래스 선언으로 상속 방지
    - private 생성자 추가로 인스턴스 생성 방지
    - AssertionError 추가로 리플렉션을 통한 인스턴스 생성 시도 방지
    - 클래스 문서화를 위한 Javadoc 주석 추가

--- src/main/java/com/studeal/team/domain/lesson/converter/LessonConverter.java
+++ src/main/java/com/studeal/team/domain/lesson/converter/LessonConverter.java
@@ -6,7 +6,18 @@ import com.studeal.team.domain.lesson.dto.LessonResponseDTO;
 
 import java.util.HashSet;
 
-public class LessonConverter {
+/**
+ * Lesson 엔티티와 DTO 간의 변환을 담당하는 유틸리티 클래스
+ * 이 클래스는 인스턴스화할 수 없습니다.
+ */
+public final class LessonConverter {
+
+    /**
+     * 유틸리티 클래스의 인스턴스화를 방지하기 위한 private 생성자
+     */
+    private LessonConverter() {
+        throw new AssertionError("유틸리티 클래스는 인스턴스화할 수 없습니다.");
+    }
 
     public static LessonResponseDTO.CreateResponse toCreateResponseDTO(Lesson lesson) {
         return LessonResponseDTO.CreateResponse.builder()
