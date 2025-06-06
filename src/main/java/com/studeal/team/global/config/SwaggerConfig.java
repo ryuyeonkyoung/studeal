0b1525520ceba3565a76d941dc086e3c5c90bcfd
류연경 <ykr.dev@gmail.com>
2025-06-07 02:12:37 +0900

    docs(api): openapi.json v2.1.0 → v2.1.1로 업데이트

--- src/main/java/com/studeal/team/global/config/SwaggerConfig.java
+++ src/main/java/com/studeal/team/global/config/SwaggerConfig.java
@@ -23,7 +23,7 @@ import java.util.Map;
     info = @Info(
         title = "Studeal API",
         description = "Studeal의 API 명세서 🌱",
-        version = "2.1.0",
+        version = "2.1.1",
         contact = @Contact(name = "Studeal Team")
     ),
     security = {
