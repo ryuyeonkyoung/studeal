0760d4b3cf3973e167513eeb744f8c9ad51092b4
류연경 <ykr.dev@gmail.com>
2025-06-06 20:04:30 +0900

    feat(board): 게시글 상세 조회(선생님) API 구현 및 MajorSubject enum JSON 변환 개선
    - GET /boards/{boardId}
    - 입찰 목록을 가격 높은 순으로 정렬하고 순위 매기는 기능 구현
    - NegotiationRepository에 fetch join 쿼리를 사용한 findByBoardIdOrderByProposedPriceDesc 메서드 추가
    - MajorSubject enum에 @JsonValue 및 @JsonCreator 어노테이션 적용하여 한글-영문 직렬화/역직렬화 지원

--- src/main/java/com/studeal/team/domain/negotiation/dao/NegotiationRepository.java
+++ src/main/java/com/studeal/team/domain/negotiation/dao/NegotiationRepository.java
@@ -2,8 +2,18 @@ package com.studeal.team.domain.negotiation.dao;
 
 import com.studeal.team.domain.negotiation.domain.Negotiation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface NegotiationRepository extends JpaRepository<Negotiation, Long> {
+    /**
+     * 게시글 ID로 관련된 모든 협상 정보 조회
+     * 학생 정보도 함께 조회 (Eager 로딩)
+     * 제안된 가격 높은 순으로 정렬
+     */
+    @Query("SELECT n FROM Negotiation n JOIN FETCH n.student s WHERE n.auctionBoard.boardId = :boardId ORDER BY n.proposedPrice DESC")
+    List<Negotiation> findByBoardIdOrderByProposedPriceDesc(Long boardId);
 }
